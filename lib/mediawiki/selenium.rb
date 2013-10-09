42c0001d06403c56e0e36866a2be6e8d93feb30e
Zeljko Filipin <zeljko.filipin@gmail.com>
2013-10-09 14:48:41 +0200

    Implemented a few changes suggested in patch set 3 of commit 86355
    Also fixed link to CREDITS file.
    
    https://gerrit.wikimedia.org/r/#/c/86355/
    Bug: 49313

--- lib/mediawiki/selenium.rb
+++ lib/mediawiki/selenium.rb
@@ -1,12 +1,12 @@
 =begin
 This file is subject to the license terms in the LICENSE file found in the
-mediawiki-selenium top-level directory of and at
+mediawiki-selenium top-level directory and at
 https://github.com/zeljkofilipin/mediawiki-selenium/blob/master/LICENSE. No part of
 mediawiki-selenium, including this file, may be copied, modified, propagated, or
 distributed except according to the terms contained in the LICENSE file.
 Copyright 2013 by the Mediawiki developers. See the CREDITS file in the
 mediawiki-selenium top-level directory and at
-https://github.com/zeljkofilipin/mediawiki-selenium/blob/master//CREDITS.
+https://github.com/zeljkofilipin/mediawiki-selenium/blob/master/CREDITS.
 =end
 
 require "mediawiki/selenium/env"
