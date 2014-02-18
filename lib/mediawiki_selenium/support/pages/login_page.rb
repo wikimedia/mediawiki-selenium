a1d7fda4af531773a748f990e294fa5adf0b0628
cmcmahon <cmcmahon@wikimedia.org>
2014-02-18 15:59:54 -0700

    Wait for login process to complete
    IE sometimes will navigate to the next page before the login
    process completes.  This causes bogus failures for example:
    
    https://wmf.ci.cloudbees.com/job/UploadWizard-commons.wikimedia.beta.wmflabs.org-windows-internet_explorer_10/482/
    
    Change-Id: I89638c67d9017dee158d958a6941d61e45499474

--- lib/mediawiki_selenium/support/pages/login_page.rb
+++ lib/mediawiki_selenium/support/pages/login_page.rb
@@ -32,5 +32,6 @@ class LoginPage
     self.password_element.when_present.send_keys(password)
     login_element.fire_event("onfocus")
     login_element.when_present.click
+    logout_link_element.when_present
   end
 end
