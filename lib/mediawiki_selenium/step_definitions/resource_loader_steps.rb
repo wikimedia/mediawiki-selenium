b83987cf40df886267f48a37386414cef40c0d62
cmcmahon <cmcmahon@wikimedia.org>
2014-03-06 17:24:40 +0100

    Make it possible to check for ResourceLoader errors anywhere
    Intended usage:
    
    Given I am on Foo Page
      Then page has no ResourceLoader errors
    
    Change-Id: I183755fb211e5b1ca0cafdbb6f570c4799cf90b5

---
+++ lib/mediawiki_selenium/step_definitions/resource_loader_steps.rb
@@ -0,0 +1,45 @@
+=begin
+This file is subject to the license terms in the LICENSE file found in the
+mediawiki_selenium top-level directory and at
+https://git.wikimedia.org/blob/mediawiki%2Fselenium/HEAD/LICENSE. No part of
+mediawiki_selenium, including this file, may be copied, modified, propagated, or
+distributed except according to the terms contained in the LICENSE file.
+Copyright 2013 by the Mediawiki developers. See the CREDITS file in the
+mediawiki_selenium top-level directory and at
+https://git.wikimedia.org/blob/mediawiki%2Fselenium/HEAD/CREDITS.
+=end
+
+Then(/^page has no ResourceLoader errors$/) do
+  @browser.execute_script("
+return (function() {
+    // Returns a string listing problem modules,
+    // or empty string if all OK (or not a MediaWiki page).
+    // MediaWiki registers many JS modules that are not loaded on average pages.
+    var i, len, state,
+        modules = mw.loader.getModuleNames(),
+        error = [],
+        missing = [],
+        ret = '';
+    if ( ( typeof mediaWiki === 'undefined' ) || !mediaWiki.loader ) {
+        return ret;
+    }
+
+    for ( i = 0, len = modules.length; i < len; i++ ) {
+        state = mw.loader.getState( modules[i] );
+        if ( state === 'error' ) {
+            error.push( modules[i] );
+        }
+        if ( state === 'missing' ) {
+            missing.push( modules[i] );
+        }
+    }
+    if ( error.length ) {
+       ret += 'Error modules: ' + error.join( '; ') + '.';
+    }
+    if ( missing.length ) {
+       ret += 'Missing modules: ' + missing.join( '; ') + '.';
+    }
+    return ret;
+}) ();
+").should == ""
+end
