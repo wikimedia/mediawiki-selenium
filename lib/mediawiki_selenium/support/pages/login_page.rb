71a10a343120774f90c123cbc9a9c8c6ca880140
Cmcmahon <cmcmahon@wikimedia.org>
2014-06-26 10:52:43 -0700

    Make the check for complete login independent of language
    Needed for /mediawiki/core tests
    
    Change-Id: I807db92670c76cf9de07d1860ebad70c6a0188dc

--- lib/mediawiki_selenium/support/pages/login_page.rb
+++ lib/mediawiki_selenium/support/pages/login_page.rb
@@ -17,7 +17,7 @@ class LoginPage
 
   div(:feedback, class: "errorbox")
   button(:login, id: "wpLoginAttempt")
-  a(:logout, href: /Special:UserLogout/)
+  li(:logout, id: "pt-logout")
   text_field(:password, id: "wpPassword1")
   a(:password_strength, text: "password strength")
   a(:phishing, text: "phishing")
