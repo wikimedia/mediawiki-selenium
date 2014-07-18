c4214cce6f28d13a749e09e93cdea8e61c33482f
Dan Duvall <dduvall@wikimedia.org>
2014-07-18 10:53:34 -0700

    Minor improvements to API-client configuration
    Memoized client instance to avoid new instances and authentication upon
    every call from `#create` and `#protect`.
    
    Lessened severity of exception to avoid a SystemExit, and tweaked the
    exception message to apply to all API operations.
    
    Change-Id: I9165e48a6b7910f1130639cdf3c153ad868ff21a

--- lib/mediawiki_selenium/support/pages/api_page.rb
+++ lib/mediawiki_selenium/support/pages/api_page.rb
@@ -4,18 +4,23 @@ class APIPage
   include PageObject
 
   def client
-    abort "Environment variable MEDIAWIKI_API_URL must be set in order to create a target page for this test" unless ENV["MEDIAWIKI_API_URL"]
+    return @client if defined?(@client)
 
-    client = MediawikiApi::Client.new ENV["MEDIAWIKI_API_URL"]
-    client.log_in ENV["MEDIAWIKI_USER"], ENV["MEDIAWIKI_PASSWORD"]
-    client
+    unless ENV["MEDIAWIKI_API_URL"]
+      raise "Environment variable MEDIAWIKI_API_URL must be set in order to use the API"
+    end
+
+    @client = MediawikiApi::Client.new(ENV["MEDIAWIKI_API_URL"])
+    @client.log_in ENV["MEDIAWIKI_USER"], ENV["MEDIAWIKI_PASSWORD"]
+
+    @client
   end
 
-  def create title, content
-    client.create_page title, content
+  def create(title, content)
+    @client.create_page title, content
   end
 
-  def protect title, reason
-    client.protect_page title, reason
+  def protect(title, reason)
+    @client.protect_page title, reason
   end
 end
