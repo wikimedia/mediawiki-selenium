301450960ccb2eb28e3d8198b5510a910dd3fcaa
Cmcmahon <cmcmahon@wikimedia.org>
2014-07-18 15:05:32 +0200

    Add call to protect method implemented in mediawiki/ruby/api
    Change-Id: I4f34342e8002e8c48a19e28d484145e334d667ef

--- lib/mediawiki_selenium/support/pages/api_page.rb
+++ lib/mediawiki_selenium/support/pages/api_page.rb
@@ -3,11 +3,19 @@ require "mediawiki_api"
 class APIPage
   include PageObject
 
-  def create title, content
+  def client
     abort "Environment variable MEDIAWIKI_API_URL must be set in order to create a target page for this test" unless ENV["MEDIAWIKI_API_URL"]
 
     client = MediawikiApi::Client.new ENV["MEDIAWIKI_API_URL"]
     client.log_in ENV["MEDIAWIKI_USER"], ENV["MEDIAWIKI_PASSWORD"]
+    client
+  end
+
+  def create title, content
     client.create_page title, content
+  end
+
+  def protect title, reason
+    client.protect_page title, reason
   end
 end
