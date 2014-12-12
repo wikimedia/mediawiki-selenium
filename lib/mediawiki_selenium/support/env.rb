d58dd5e615ce6e538c26d7351afb3f0483ee5d97
Dan Duvall <dduvall@wikimedia.org>
2014-12-11 17:43:16 -0800

    Minor cruft removal
    Removed URL and SauceHelper modules as they're no longer needed.
    
    Change-Id: I525ce51232758099889b2d4e0270a04ece511b38

--- lib/mediawiki_selenium/support/env.rb
+++ lib/mediawiki_selenium/support/env.rb
@@ -15,12 +15,10 @@ require "page-object/page_factory"
 require "watir-webdriver"
 
 require "mediawiki_selenium/support/modules/api_helper"
-require "mediawiki_selenium/support/modules/sauce_helper"
 require "mediawiki_selenium/support/modules/strict_pending"
 
 World { MediawikiSelenium::Environment.load_default }
 
 World(MediawikiSelenium::ApiHelper)
 World(MediawikiSelenium::PageFactory)
-World(MediawikiSelenium::SauceHelper)
 World(MediawikiSelenium::StrictPending)
