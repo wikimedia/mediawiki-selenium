cf6ae13a668f54d74359a5713fca1f63aee1aa22
Dan Duvall <dduvall@wikimedia.org>
2015-02-20 16:21:31 -0800

    Corrected Style/BlockComments RuboCop offenses
    Change-Id: I5119a0f92d4fb6d56f9ec7da58cf437fb84b762f

--- lib/mediawiki_selenium/support/hooks.rb
+++ lib/mediawiki_selenium/support/hooks.rb
@@ -1,14 +1,3 @@
-=begin
-This file is subject to the license terms in the LICENSE file found in the
-mediawiki_selenium top-level directory and at
-https://git.wikimedia.org/blob/mediawiki%2Fselenium/HEAD/LICENSE. No part of
-mediawiki_selenium, including this file, may be copied, modified, propagated, or
-distributed except according to the terms contained in the LICENSE file.
-Copyright 2013 by the Mediawiki developers. See the CREDITS file in the
-mediawiki_selenium top-level directory and at
-https://git.wikimedia.org/blob/mediawiki%2Fselenium/HEAD/CREDITS.
-=end
-
 Before("@custom-browser") do |scenario|
   @scenario = scenario
 end
