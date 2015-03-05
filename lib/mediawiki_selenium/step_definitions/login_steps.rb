794baf35197fab921299ff82b012365127ae35bd
Dan Duvall <dduvall@wikimedia.org>
2015-03-05 14:10:17 -0800

    Fix regex pattern in shared "I am logged in" step
    Change-Id: Icb5fb655da09f50d3f4b8ece20bad5e8d061af6c

--- lib/mediawiki_selenium/step_definitions/login_steps.rb
+++ lib/mediawiki_selenium/step_definitions/login_steps.rb
@@ -1,4 +1,4 @@
-Given(/^I am logged in(?: as (\w+))$/) do |user|
+Given(/^I am logged in(?: as (\w+))?$/) do |user|
   as_user(user) do |user, password|
     visit(LoginPage).login_with(user, password)
   end
