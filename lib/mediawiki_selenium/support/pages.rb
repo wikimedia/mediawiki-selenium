e40a6b8feff7ede6ffc2ff0659294dcc71823653
Dan Duvall <dduvall@wikimedia.org>
2015-06-10 16:31:14 -0700

    Remove `APIPage` page object
    Having a page object for accessing the API is an unnecessary abstraction
    especially given we now have `MediawikiSelenium::ApiHelper` which
    provides direct access to an instantiated API client.
    
    Change-Id: Ifa2d1f5984d52b34872bc9f7fe8992ea35b9ec79

--- lib/mediawiki_selenium/support/pages.rb
+++ lib/mediawiki_selenium/support/pages.rb
@@ -1,4 +1,3 @@
-require 'mediawiki_selenium/support/pages/api_page'
 require 'mediawiki_selenium/support/pages/login_page'
 require 'mediawiki_selenium/support/pages/random_page'
 require 'mediawiki_selenium/support/pages/reset_preferences_page'
