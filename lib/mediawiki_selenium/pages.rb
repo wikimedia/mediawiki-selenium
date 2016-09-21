d777d9926287b08b79ac34d88e4bc38b92a1cd8a
Željko Filipin <zeljko.filipin@gmail.com>
2016-09-21 19:38:50 +0200

    Helper that allows you to query whether JavaScript module has loaded
    Bug: T146292
    Change-Id: Id1c5d91dfdc285e859e553d8a03c2bd29200899c

--- lib/mediawiki_selenium/pages.rb
+++ lib/mediawiki_selenium/pages.rb
@@ -1,3 +1,4 @@
+require 'mediawiki_selenium/pages/article_page'
 require 'mediawiki_selenium/pages/login_page'
 require 'mediawiki_selenium/pages/random_page'
 require 'mediawiki_selenium/pages/reset_preferences_page'
